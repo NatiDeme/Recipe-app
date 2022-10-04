31550f99ec32a97168651e8359581bc6c20f35e5
NatiDeme <demelashnatnael@gmail.com>
2022-10-04 15:28:20 +0300

    Add environment for the food list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :foods, only: [:new, :create, :destroy]
+  # get '/foods', to: 'foods#index'
+  # get '/foods/new', to: 'foods#new'
+
+  resources :foods, only: [:index, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
