eb4fa3e075ee73da9850deaf4551310d66270260
NatiDeme <demelashnatnael@gmail.com>
2022-10-04 23:27:48 +0300

    Add new recipe and detail pages with controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,30 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+  def new
+    @recipe = Recipe.new
+  end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.valid?
+      @recipe.save
+      flash[:notice] = 'New Recipe Created Successfully'
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Successfully removed the Recipe.'
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   root 'users#index'
   get '/users', to: 'users#index', as: 'users'
-  get '/recipes', to: 'recipes#index', as: 'recipes'
-  get '/recipes/:id', to: 'recipes#show', as: 'recipes_detail'
+  # get '/recipes', to: 'recipes#index', as: 'recipes'
+  # get '/recipes/:id', to: 'recipes#show', as: 'recipes_detail'
 
   devise_scope :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # get '/foods/new', to: 'foods#new'
 
   resources :foods, only: %i[index new create destroy]
+  resources :recipes, only: %i[index new create show destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
