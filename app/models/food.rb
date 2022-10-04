59bbb200c02218e5c181052e8ae6209bc99ee171
jheart-vic <adebowalevictorjheart@gmail.com>
2022-10-04 09:11:41 +0100

    Modify the food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :recipe_foods, foreign_key: :post_id, dependent: :delete_all
+  has_many :recipe_foods, dependent: :delete_all
 
   validates :measurement_unit, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
