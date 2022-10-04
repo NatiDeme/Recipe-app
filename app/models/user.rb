37081477f9f1fcfaf171d66fe1519eed2da9d4cd
NatiDeme <demelashnatnael@gmail.com>
2022-10-04 04:29:39 +0300

    Add recipe list page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
+
+  has_many :foods, foreign_key: 'user_id'
+  has_many :recipes, foreign_key: 'author_id'
+  has_many :recipe_foods, foreign_key: 'user_id'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
   devise_for :users
   root 'users#index'
   get '/users', to: 'users#index', as: 'users'
+  get '/recipes', to: 'recipes#index', as: 'recipes'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
