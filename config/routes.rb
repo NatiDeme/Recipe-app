1c03495d591bf82eb13e3fcdf8bf8affc8969469
NatiDeme <demelashnatnael@gmail.com>
2022-10-04 04:43:54 +0300

    Add setup to recipe detail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  devise_for :users
+   devise_for :users
+
   root 'users#index'
   get '/users', to: 'users#index', as: 'users'
   get '/recipes', to: 'recipes#index', as: 'recipes'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipes_detail'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
